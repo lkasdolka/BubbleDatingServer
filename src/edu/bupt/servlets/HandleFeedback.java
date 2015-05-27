e21f2dc255190a8c2e687a9192e4808df0acc1be
lkasdolka <liukeang@163.com>
2015-05-27 15:54:33 +0800

    添加处理反馈信息页

---
+++ src/edu/bupt/servlets/HandleFeedback.java
@@ -0,0 +1,91 @@
+package edu.bupt.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import edu.bupt.util.SqlTool;
+
+public class HandleFeedback extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public HandleFeedback() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String userName = request.getParameter("username");
+		String data = request.getParameter("data");
+		System.out.println("user:"+userName+" submit a feedback:\n"+data);
+		
+		response.setContentType("text/json");
+		PrintWriter out = response.getWriter();
+		int result = SqlTool.handFeedback(userName, data);
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		
+		if(result == 1){
+			map.put("result", 1);
+		}else{
+			map.put("result", 0);
+		}
+		JSONObject res = new JSONObject(map);
+		out.print(res);
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
--- src/edu/bupt/util/SqlTool.java
+++ src/edu/bupt/util/SqlTool.java
@@ -31,15 +31,14 @@ public class SqlTool {
 	private final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	private final static String DB_NAME = "bubble_datiing";
 	private final static double EPSILON = 0.00001;
-	
+
 	private static Connection connection = null;
 	private static PreparedStatement preparedStatement = null;
 	private static ResultSet resultSet = null;
 	private static BasicDataSource ds = null;
-	
+
 	public final static String STATUS_KEY = "status";
-	
-	
+
 	public static void main(String[] args) {
 		try {
 			SqlTool.initConnectionPool();
@@ -47,15 +46,15 @@ public class SqlTool {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		updateUserLoc("test3",22.2,100.0);
+
+		handFeedback("aaa", "I love this app");
 	}
-	
-	public static BasicDataSource getInstance(){
+
+	public static BasicDataSource getInstance() {
 		return ds;
 	}
-	
-	public static void initConnectionPool() throws SQLException{
+
+	public static void initConnectionPool() throws SQLException {
 		ds = new BasicDataSource();
 		ds.setDriverClassName(JDBC_DRIVER);
 		ds.setUrl(URL2);
@@ -63,14 +62,14 @@ public class SqlTool {
 		ds.setPassword(PW);
 		ds.setDefaultAutoCommit(false);
 	}
-	
+
 	public static void shutdownDataSource(DataSource ds) throws SQLException {
-        BasicDataSource bds = (BasicDataSource) ds;
-        bds.close();
-    }
-	
-	public static void recycleRes(){
-		if(resultSet != null){
+		BasicDataSource bds = (BasicDataSource) ds;
+		bds.close();
+	}
+
+	public static void recycleRes() {
+		if (resultSet != null) {
 			try {
 				resultSet.close();
 			} catch (SQLException e) {
@@ -78,7 +77,7 @@ public class SqlTool {
 				e.printStackTrace();
 			}
 		}
-		if(preparedStatement != null){
+		if (preparedStatement != null) {
 			try {
 				preparedStatement.close();
 			} catch (SQLException e) {
@@ -86,7 +85,7 @@ public class SqlTool {
 				e.printStackTrace();
 			}
 		}
-		if(connection != null){
+		if (connection != null) {
 			try {
 				connection.close();
 				System.out.println("end the connection");
@@ -96,7 +95,7 @@ public class SqlTool {
 			}
 		}
 	}
-	
+
 	@Deprecated
 	public static void connectMysql() {
 
@@ -107,29 +106,30 @@ public class SqlTool {
 
 			connection = DriverManager.getConnection(URL, USER, PW);
 			System.out.println("make connection");
-			System.out.println("connection name:"+connection.getClass().getName());
+			System.out.println("connection name:"
+					+ connection.getClass().getName());
 
 			connection.setCatalog(DB_NAME);
 			System.out.println("set database");
-			
+
 			connection.setAutoCommit(false);
 			System.out.println("ready for transaction");
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
 	}
-	
+
 	@Deprecated
-	public static void endConnection(){
+	public static void endConnection() {
 		try {
 			connection.setAutoCommit(true);
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		if(resultSet != null){
+		if (resultSet != null) {
 			try {
 				resultSet.close();
 			} catch (SQLException e) {
@@ -137,7 +137,7 @@ public class SqlTool {
 				e.printStackTrace();
 			}
 		}
-		if(preparedStatement != null){
+		if (preparedStatement != null) {
 			try {
 				preparedStatement.close();
 			} catch (SQLException e) {
@@ -145,7 +145,7 @@ public class SqlTool {
 				e.printStackTrace();
 			}
 		}
-		if(connection != null){
+		if (connection != null) {
 			try {
 				connection.close();
 				System.out.println("end the connection");
@@ -155,20 +155,20 @@ public class SqlTool {
 			}
 		}
 	}
-	
-	public static void selectAll(String tableName){
+
+	public static void selectAll(String tableName) {
 		try {
 			connection = SqlTool.getInstance().getConnection();
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		String sql = "select * from "+tableName;
+		String sql = "select * from " + tableName;
 		System.out.println(sql);
 		try {
 			preparedStatement = connection.prepareStatement(sql);
 			resultSet = preparedStatement.executeQuery();
-			while(resultSet.next()){
+			while (resultSet.next()) {
 				System.out.println(resultSet.getInt("u_id"));
 				System.out.println(resultSet.getString("u_name"));
 				System.out.println(resultSet.getString("u_pw"));
@@ -176,140 +176,163 @@ public class SqlTool {
 				System.out.println(resultSet.getString("u_gender"));
 				System.out.println(resultSet.getString("u_online"));
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
+		} finally {
 			SqlTool.recycleRes();
 		}
 	}
-	
-	public static boolean isExist(String colName,String value){
-		//u_name is string , here needs a quote
-		
-		String sql = "select "+colName+" from user_info where "+colName+"='"+value+"'";
+
+	public static boolean isExist(String colName, String value) {
+		// u_name is string , here needs a quote
+
+		String sql = "select " + colName + " from user_info where " + colName
+				+ "='" + value + "'";
 		System.out.println(sql);
 		try {
 			connection = SqlTool.ds.getConnection();
 			System.out.println("acquire the connection");
-			if(connection == null) System.out.println("connection is null.");
-			else System.out.println("connection is not null");
+			if (connection == null)
+				System.out.println("connection is null.");
+			else
+				System.out.println("connection is not null");
 			preparedStatement = connection.prepareStatement(sql);
 			resultSet = preparedStatement.executeQuery();
-			if(resultSet.next()){
+			if (resultSet.next()) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
+		} finally {
 			SqlTool.recycleRes();
 		}
 		System.out.println("exception during query");
 		return false;
 	}
-	
+
 	public static User isExistMultiParam(ArrayList<String> colNames,
-			ArrayList<String> values,String username) {
+			ArrayList<String> values, String username) {
 		if (colNames == null || values == null
 				|| colNames.size() != values.size()) {
 			return null;
 		}
 		String query = "select * from user_info where ";
 		for (int i = 0; i < colNames.size(); i++) {
-			if(i!=colNames.size()-1){
-				query += colNames.get(i)+"='"+values.get(i)+"' and ";
-			}else{
-				query += colNames.get(i)+"='"+values.get(i)+"'";
+			if (i != colNames.size() - 1) {
+				query += colNames.get(i) + "='" + values.get(i) + "' and ";
+			} else {
+				query += colNames.get(i) + "='" + values.get(i) + "'";
 			}
-			
+
 		}
 		System.out.println(query);
 		try {
 			connection = SqlTool.getInstance().getConnection();
 			preparedStatement = connection.prepareStatement(query);
-			if(preparedStatement.execute()){
+			if (preparedStatement.execute()) {
 				resultSet = preparedStatement.getResultSet();
 				System.out.println("get full user info");
-				if(resultSet.next()){
+				if (resultSet.next()) {
 					System.out.println("get target user info");
-					preparedStatement = connection.prepareStatement("select * from user_info where u_name='"+username+"'");
-					if(preparedStatement.execute()){
+					preparedStatement = connection
+							.prepareStatement("select * from user_info where u_name='"
+									+ username + "'");
+					if (preparedStatement.execute()) {
 						resultSet = preparedStatement.getResultSet();
-						if(resultSet.next()){
-							User userInfo = new User(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), null, false);
-							System.out.println("user entity in getUser:"+userInfo.toString());
+						if (resultSet.next()) {
+							User userInfo = new User(resultSet.getInt(1),
+									resultSet.getString(2),
+									resultSet.getString(3),
+									resultSet.getString(4),
+									resultSet.getString(5), null, false);
+							System.out.println("user entity in getUser:"
+									+ userInfo.toString());
 							return userInfo;
-						}else{
+						} else {
 							System.out.println("cannot find target user");
 						}
 					}
-					
+
 				}
-			}else{
+			} else {
 				System.out.println("no result");
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
+		} finally {
 			SqlTool.recycleRes();
 		}
-		
+
 		return null;
 	}
-	
-	
-	public static ResponseStatus addUser(String uname,String pw,String email,String gender,String image){
-		if(isExist("u_name",uname)){
-			System.out.println(uname+ " already exists!");
+
+	public static ResponseStatus addUser(String uname, String pw, String email,
+			String gender, String image) {
+		if (isExist("u_name", uname)) {
+			System.out.println(uname + " already exists!");
 			return ResponseStatus.USER_NAME_DUPLICATE;
 		}
-		if(isExist("u_email", email)){
-			System.out.println(email+ " already exists!");
+		if (isExist("u_email", email)) {
+			System.out.println(email + " already exists!");
 			return ResponseStatus.EMAIL_DUPLICATE;
 		}
-		
-		String sql = "insert into user_info (u_id,u_name,u_pw,u_email,u_gender,u_image) values ("+null+",'"+uname+"','"+pw+"','"+email+"','"+gender+"',"+null+")";
+
+		String sql = "insert into user_info (u_id,u_name,u_pw,u_email,u_gender,u_image) values ("
+				+ null
+				+ ",'"
+				+ uname
+				+ "','"
+				+ pw
+				+ "','"
+				+ email
+				+ "','"
+				+ gender + "'," + null + ")";
 		System.out.println(sql);
 		try {
 			connection = SqlTool.getInstance().getConnection();
 			preparedStatement = connection.prepareStatement(sql);
-			if(!preparedStatement.execute()){
+			if (!preparedStatement.execute()) {
 				int count = preparedStatement.getUpdateCount();
-				if(count > 0){
-					System.out.println("insert successfully,"+count+" line(s) updated");
+				if (count > 0) {
+					System.out.println("insert successfully," + count
+							+ " line(s) updated");
 					connection.commit();
 					return ResponseStatus.OK;
 				}
-			}else{
+			} else {
 				System.out.println("insert fails.");
 				return ResponseStatus.UNKNOWN_ERROR;
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
+		} finally {
 			SqlTool.recycleRes();
 		}
 		System.out.println("exception happened during add user.");
 		return ResponseStatus.UNKNOWN_ERROR;
-		
+
 	}
-	
-	public static ArrayList<JSONObject> queryPeopleAround(){
+
+	public static ArrayList<JSONObject> queryPeopleAround() {
 		ArrayList<JSONObject> res = new ArrayList<JSONObject>();
 		try {
-			// issue : group by cannot sort duplicate items desc with time ,and present the oldest item
+			// issue : group by cannot sort duplicate items desc with time ,and
+			// present the oldest item
 			connection = SqlTool.getInstance().getConnection();
-			preparedStatement = connection.prepareStatement("select distinct  invitation.u_id,u_invi,u_posttime,u_name,u_gender,u_loc_lat,u_loc_long from invitation  inner join user_info where  user_info.u_id = invitation.u_id group by invitation.u_id order by u_posttime desc ;");
-			if(preparedStatement.execute()){
+			preparedStatement = connection
+					.prepareStatement("select distinct  invitation.u_id,u_invi,u_posttime,u_name,u_gender,u_loc_lat,u_loc_long from invitation  inner join user_info where  user_info.u_id = invitation.u_id group by invitation.u_id order by u_posttime desc ;");
+			if (preparedStatement.execute()) {
 				resultSet = preparedStatement.getResultSet();
-				while(resultSet.next()){
-					if(Math.abs(resultSet.getDouble(6))<EPSILON || Math.abs(resultSet.getDouble(7))<EPSILON){
+				while (resultSet.next()) {
+					if (Math.abs(resultSet.getDouble(6)) < EPSILON
+							|| Math.abs(resultSet.getDouble(7)) < EPSILON) {
 						continue;
 					}
 					JSONObject item = new JSONObject();
@@ -326,19 +349,20 @@ public class SqlTool {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
+		} finally {
 			SqlTool.recycleRes();
 		}
-		
+
 		return res;
-		
+
 	}
-	
-	public static void updateUserLoc(String userName, double lat, double lon){
+
+	public static void updateUserLoc(String userName, double lat, double lon) {
 		try {
 			connection = SqlTool.getInstance().getConnection();
 			System.out.println("get a connection from connection pool");
-			preparedStatement = connection.prepareStatement("update user_info set u_loc_lat=?,u_loc_long=? where u_name=?");
+			preparedStatement = connection
+					.prepareStatement("update user_info set u_loc_lat=?,u_loc_long=? where u_name=?");
 			preparedStatement.setDouble(1, lat);
 			preparedStatement.setDouble(2, lon);
 			preparedStatement.setString(3, userName);
@@ -349,42 +373,65 @@ public class SqlTool {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
+		} finally {
 			SqlTool.recycleRes();
 		}
-		
-		
+
 	}
-	
-	
+
+	public static int handFeedback(String username, String content) {
+
+		try {
+			connection = SqlTool.getInstance().getConnection();
+			String insertSql = "insert into feedback (u_name,u_content) values (?,?)";
+			preparedStatement = connection.prepareStatement(insertSql);
+			preparedStatement.setString(1, username);
+			preparedStatement.setString(2, content);
+
+			boolean flag = preparedStatement.execute();
+			if (!flag) {
+				int count = preparedStatement.getUpdateCount();
+				if (count != 0) {
+					System.out
+							.println("insert feed back success, insert count:"
+									+ count);
+					connection.commit();
+					return 1;
+
+				} else {
+					System.out.println("insert failed.");
+					return 0;
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+
+	}
+
 	public static enum ResponseStatus {
-		OK(0),
-		USER_NAME_DUPLICATE(1),
-		EMAIL_DUPLICATE(2),
-		UNKNOWN_ERROR(3),
-		UNKOWN_USERNAME(4),
-		USERNAME_PASSWORD_UNCOMPATIBLE(5),
-		USER_NOT_ON_HX(6);
-		
+		OK(0), USER_NAME_DUPLICATE(1), EMAIL_DUPLICATE(2), UNKNOWN_ERROR(3), UNKOWN_USERNAME(
+				4), USERNAME_PASSWORD_UNCOMPATIBLE(5), USER_NOT_ON_HX(6);
+
 		private final int value;
-		
-		ResponseStatus(int x){
+
+		ResponseStatus(int x) {
 			value = x;
-		} 
-		
-		public int getValue(){
+		}
+
+		public int getValue() {
 			return value;
 		}
-		
-	}	
-	
+
+	}
+
 	public static void printDataSourceStats(DataSource ds) {
-        BasicDataSource bds = (BasicDataSource) ds;
-        System.out.println("NumActive: " + bds.getNumActive());
-        System.out.println("NumIdle: " + bds.getNumIdle());
-    }
-	
-	
-	
+		BasicDataSource bds = (BasicDataSource) ds;
+		System.out.println("NumActive: " + bds.getNumActive());
+		System.out.println("NumIdle: " + bds.getNumIdle());
+	}
 
 }
