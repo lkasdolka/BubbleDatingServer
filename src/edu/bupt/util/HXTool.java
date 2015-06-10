70044b808d622ee5b47a378424ff23a02a8353fa
lkasdolka <liukeang@163.com>
2015-06-10 16:46:51 +0800

    封装httpclient支持https

--- src/edu/bupt/util/HXTool.java
+++ src/edu/bupt/util/HXTool.java
@@ -2,9 +2,16 @@ package edu.bupt.util;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.net.ssl.SSLContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 
@@ -17,10 +24,20 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
@@ -30,21 +47,23 @@ public class HXTool {
 	private static final String CLIENT_SECRET = "YXA6zNTcEdfFaANjcQnP3IdnpeV-LIc";
 	private static final String GET_ACCESS_URL = "https://a1.easemob.com/halfdog/bubbledating/token";
 	private static final String REGISTER_URL = "https://a1.easemob.com/halfdog/bubbledating/users";
-//	private static String ACCESS_TOKEN = "";
-
+	private static final String CER_PATH = "F:\\Program Files\\Java\\jre1.8.0_31\\lib\\security\\cacerts";
+	private static final String CER_PATH2 = "F:\\Program Files\\Java\\jre1.8.0_31\\lib\\security\\cacerts";
+	
 	public static void main(String[] args) {
-		System.setProperty("javax.net.ssl.trustStore", "F:\\Program Files\\Java\\jdk1.8.0_31\\lib\\cacerts");
+//		System.setProperty("javax.net.ssl.trustStore", CER_PATH);
 //		System.out.println(getAccessToken());
 //		int resCode = registerHXUser("aaa", "aaa");
 //		if(resCode == 200)System.out.println("register successfully");
 //		else System.out.println("certain problem occurred");
-		System.out.println(getUser("aaas"));
+		getAccessToken();
+		System.out.println(getUser("aaa"));
 	}
 
 	public static String getAccessToken() {
-		System.setProperty("javax.net.ssl.trustStore", "F:\\Program Files\\Java\\jdk1.8.0_31\\lib\\cacerts");
+//		System.setProperty("javax.net.ssl.trustStore", CER_PATH);
 		String res = null;
-		CloseableHttpClient httpclient = HttpClients.createDefault();
+		CloseableHttpClient httpclient = getHttpClient();
 		HttpPost httpPost = new HttpPost(GET_ACCESS_URL);
 		JSONObject body = new JSONObject();
 		body.put("grant_type", "client_credentials");
@@ -89,8 +108,10 @@ public class HXTool {
 		String ans = null;
 		int statusCode = -1;
 		
-		CloseableHttpClient httpclient = HttpClients.createDefault();
+//		CloseableHttpClient httpclient = HttpClients.createDefault();
+		CloseableHttpClient httpclient = getHttpClient();
 		HttpGet httpGet = new HttpGet(REGISTER_URL+"/"+username);
+		System.out.println("get user:"+REGISTER_URL+"/"+username);
 		httpGet.setHeader("Authorization","Bearer "+ConstantArgs.HX_ACCESS_TOKEN);
 		CloseableHttpResponse response1 = null;
 		try {
@@ -133,7 +154,7 @@ public class HXTool {
 		int statusCode = -1;
 		int registerStatus = -1;
 		
-		CloseableHttpClient httpclient = HttpClients.createDefault();
+		CloseableHttpClient httpclient = getHttpClient();
 		HttpPost httpPost = new HttpPost(REGISTER_URL);
 		JSONObject body = new JSONObject();
 		body.put("username", user);
@@ -176,7 +197,7 @@ public class HXTool {
 
 	public static String get(String url) {
 		String res = null;
-		CloseableHttpClient httpclient = HttpClients.createDefault();
+		CloseableHttpClient httpclient = getHttpClient();
 		HttpGet httpGet = new HttpGet(url);
 		CloseableHttpResponse response1 = null;
 		try {
@@ -210,7 +231,7 @@ public class HXTool {
 
 	public static String post(String url, String username, String pw) {
 		String res = null;
-		CloseableHttpClient httpclient = HttpClients.createDefault();
+		CloseableHttpClient httpclient = getHttpClient();
 		HttpPost httpPost = new HttpPost(url);
 		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
 		nvps.add(new BasicNameValuePair("username", username));
@@ -239,4 +260,38 @@ public class HXTool {
 		}
 		return res;
 	}
+	private static CloseableHttpClient getHttpClient() {  
+        RegistryBuilder<ConnectionSocketFactory> registryBuilder = RegistryBuilder.<ConnectionSocketFactory>create();  
+        ConnectionSocketFactory plainSF = new PlainConnectionSocketFactory();  
+        registryBuilder.register("http", plainSF);  
+//指定信任密钥存储对象和连接套接字工厂  
+        try {  
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());  
+            //信任任何链接  
+            TrustStrategy anyTrustStrategy = new TrustStrategy() {  
+                @Override  
+                public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {  
+                    return true;  
+                }
+
+            };  
+            SSLContext sslContext = SSLContexts.custom().useTLS().loadTrustMaterial(trustStore, anyTrustStrategy).build();  
+            LayeredConnectionSocketFactory sslSF = new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);  
+            registryBuilder.register("https", sslSF);  
+        } catch (KeyStoreException e) {  
+            throw new RuntimeException(e);  
+        } catch (KeyManagementException e) {  
+            throw new RuntimeException(e);  
+        } catch (NoSuchAlgorithmException e) {  
+            throw new RuntimeException(e);  
+        }  
+        Registry<ConnectionSocketFactory> registry = registryBuilder.build();  
+        //设置连接管理器  
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(registry);  
+//      connManager.setDefaultConnectionConfig(connConfig);  
+//      connManager.setDefaultSocketConfig(socketConfig);  
+        //构建客户端  
+        return HttpClientBuilder.create().setConnectionManager(connManager).build();  
+    }  
+	
 }
